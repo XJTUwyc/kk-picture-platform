44893729721382e775557f06a64e1039e3ce5e27
XJTUwyc <xjtuwyc@stu.xjtu.edu.cn>
2025-06-18 03:41:28 +0800

    后端项目初始化

---
+++ src/test/java/com/kk/kkpicturebackend/KkPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kk.kkpicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkPictureBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
